0a44f47737b6435826a827c0fca7f44365c124d2
Trevor Damoyi <trevordamoyi307@gmail.com>
2025-07-04 20:30:43 +0200

    Bug Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
 
       <div id='skills_section' className="z-10 w-full font-sans mt-8 items-center flex-col justify-center">
         <h3 id="skills_title" className='text-2xl font-semibold text-purple-700 dark:text-purple-400'>Skills</h3>
-        <ul className='flex-wrap gap-4 p-4 m-5 rounded list-none justify-center flex-row sm:flex-col md:flex-row lg:flex-row'>
+        <ul className='flex-col gap-4 p-4 m-5 rounded list-none justify-center sm:flex-col md:flex-row'>
           <li className='text-base text-gray-700 dark:text-gray-300 border-2 rounded-md border-purple-700 dark:border-purple-400'>
             JavaScript
           </li>
